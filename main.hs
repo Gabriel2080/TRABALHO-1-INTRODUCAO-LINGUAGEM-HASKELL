cb73165a287432435fedde2beaac7331b370bbcf
Gabriel Ezequiel <gabriel.ezequiel@pucpr.edu.br>
2022-08-25 20:15:43 -0300

    melhorias

--- main.hs
+++ main.hs
@@ -23,7 +23,7 @@ resto x y = x `mod` y
 maior :: Double -> Double -> Double
 maior x y
     | x < y = y
-    | x > y = x
+    | otherwise = x
 
 precoMaior :: Double -> Double -> Double -> Double -> Double
 precoMaior x y z w = maior (maior x y) (maior z w)
@@ -33,29 +33,29 @@ precoMaior x y z w = maior (maior x y) (maior z w)
 impar :: Int -> Int -> Bool
 impar x y = (x * y) `mod` 2 == 1
 
--- Em Haskell existe o tipo par cuja assinatura tem a seguinte forma: 𝑝𝑎𝑟 ∷ (𝐼𝑛𝑡,𝐼𝑛𝑡). Escreva uma função em Haskell que devolva a soma dos componentes de um par de inteiros.
+-- 7. Em Haskell existe o tipo par cuja assinatura tem a seguinte forma: 𝑝𝑎𝑟 ∷ (𝐼𝑛𝑡,𝐼𝑛𝑡). Escreva uma função em Haskell que devolva a soma dos componentes de um par de inteiros.
 
 par :: (Int, Int) -> Int
 par (x, y) = x + y
 
--- 7. Escreva uma função em Haskell que receba números reais (double) e devolva o resultado da equação 𝑥**2 + (𝑦 / 2) + 𝑧.
+-- 8. Escreva uma função em Haskell que receba números reais (double) e devolva o resultado da equação 𝑥**2 + (𝑦 / 2) + 𝑧.
 
 equacao :: Double -> Double -> Double -> Double
 equacao x y z = x**2 + (y / 2) + z
 
--- 8. Escreva uma função em Haskell chamada diagnostico que receba o peso do aluno e imprima um diagnóstico de obesidade, segundo a tabela que pode ser encontrada no link: Sobrepeso, obesidade e obesidade mórbida: entenda a diferença entre os três termos (cuidadospelavida.com.br). Observe que este diagnóstico é meramente estatístico e não tem nenhum valor real, está sendo usado nesta questão apenas para a definição das faixas. Todo e qualquer diagnóstico deve ser feito por um profissional médico
+-- Escreva uma função em Haskell chamada diagnostico que receba o peso e a altura do aluno e imprima um diagnóstico de obesidade, segundo a tabela que pode ser encontrada no link: Sobrepeso,  obesidade  e  obesidade  mórbida:  entenda  a  diferença  entre  os  três  termos (cuidadospelavida.com.br).  Observe  que  este  diagnóstico  é  meramente  estatístico  e  não tem nenhum valor real, está sendo usado nesta questão apenas para a definição das faixas. Todo e qualquer diagnóstico deve ser feito por um profissional médico.
 
-diagnostico :: Double -> String
-diagnostico x
-    | x / (1.70**2) < 17 = "Muito abaixo do peso"
-    | x / (1.70**2) < 18.49 = "Abaixo do peso"
-    | x / (1.70**2) < 24.99 = "Peso normal"
-    | x / (1.70**2) < 29.99 = "Sobrepeso"
-    | x / (1.70**2) < 34.99 = "Obesidade Ieve"
-    | x / (1.70**2) < 39.99 = "Obesidade severa"
+diagnostico :: Double -> Double -> String
+diagnostico x y
+    | x / (y**2) < 17 = "Muito abaixo do peso"
+    | x / (y**2) < 18.49 = "Abaixo do peso"
+    | x / (y**2) < 24.99 = "Peso normal"
+    | x / (y**2) < 29.99 = "Sobrepeso"
+    | x / (y**2) < 34.99 = "Obesidade Ieve"
+    | x / (y**2) < 39.99 = "Obesidade severa"
     | otherwise = "Obesidade morbida"
 
--- 9. Escreva uma função em Haskell chamada bissexto que receba um ano e devolva True se o ano for bisexto sabendo que anos bissextos obedecem a seguinte regra: 𝑇𝑜𝑑𝑜𝑠 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠𝑒𝑗𝑎𝑚 𝑑𝑖𝑣𝑖𝑠í𝑣𝑒𝑖𝑠 𝑝𝑜𝑟 4 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 100 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 400 1997 não é bissexto, 1900 não é bissexto e 2000 é bissexto.
+-- 10. Escreva uma função em Haskell chamada bissexto que receba um ano e devolva True se o ano for bisexto sabendo que anos bissextos obedecem a seguinte regra: 𝑇𝑜𝑑𝑜𝑠 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠𝑒𝑗𝑎𝑚 𝑑𝑖𝑣𝑖𝑠í𝑣𝑒𝑖𝑠 𝑝𝑜𝑟 4 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 100 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 400 1997 não é bissexto, 1900 não é bissexto e 2000 é bissexto.
 
 bissexto :: Int -> Bool
 bissexto x
@@ -82,6 +82,6 @@ main = do
   putStr "equacao: entrada: 1 2 3; resultado: "
   print(equacao 1 2 3)
   putStr "diagnostico: entrada: 46; resultado: "
-  print(diagnostico 46)
+  print(diagnostico 46 1.70)
   putStr "bissexto: entrada: 2024; resultado: "
   print(bissexto 2024)
